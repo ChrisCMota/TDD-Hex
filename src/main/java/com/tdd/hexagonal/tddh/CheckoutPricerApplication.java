e11edb0c846536a738429427662e08d87b69596d
ChrisCMota <100359859+ChrisCMota@users.noreply.github.com>
2025-02-05 19:56:54 +0000

    first commit

---
+++ src/main/java/com/tdd/hexagonal/tddh/CheckoutPricerApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.hexagonal.tddh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutPricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutPricerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdd/hexagonal/tddh/CheckoutPricerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.hexagonal.tddh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutPricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
